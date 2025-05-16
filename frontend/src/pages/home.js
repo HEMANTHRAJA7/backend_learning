2e18bf4c7b9e5cdc5d4d24160a6110420db9d57d
Hemanth Raja <hemanthraja2004@gmail.com>
2025-05-16 09:52:41 +0530

    Connection established

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const home = () => {
+import WorkoutDetails from '../components/workoutDetails'
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts');
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
-    <div>home</div>
-  )
-}
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default home
+export default Home;
